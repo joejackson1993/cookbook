1a1442e420125bdcd16d89c9349ea767e6c889c7
Joseph.Jackson <midnjackson@gmail.com>
2018-04-22 07:48:30 -0400

    added tinyMce via node

--- src/components/RecipeOverview.js
+++ src/components/RecipeOverview.js
@@ -10,13 +10,14 @@ class RecipeOverview extends Component {
   }
 
 	render (){
-
     const data = this.state.data;
 
 		return (
 			<div className="Overview-box">
 				<h3 className="Recipe-title">{data.title}</h3>
         <p className="Rating-box">{"Rating: " + data.rating}</p>
+				//TODO need to figure out how to handle the HTML for the description to add the image tags to it so I can have them be waypoints....
+				//
         <pre className="Recipe-description">{data.description}</pre>
    		</div>
   	);
